e5cdbc757326f5d8c93b150b3314d03646e9fcfb
Nicholas Primiano <njp280@nyu.edu>
2016-07-26 04:30:38 -0400

    no message

---
+++ Assets/Scripts/SpawnZombie.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnZombie : MonoBehaviour
+{
+
+	public GameObject zombie;
+	public GameObject player;
+
+	void spawnZombie ()
+	{
+		Instantiate (zombie, player.transform.position, Quaternion.identity);
+	
+	}
+
+	void Start ()
+	{
+		InvokeRepeating ("spawnZombie", 2f, 2f);
+	}
+
+	void Update ()
+	{
+		
+	}
+
+}
---
+++ Assets/Scripts/ZombieMove.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieMove : MonoBehaviour
+{
+	private Transform destination;
+	public float zombieMoveSpeed = 2;
+
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		destination = GameObject.FindGameObjectWithTag ("Player").transform;
+
+		if (Vector3.Distance (transform.position, destination.position) > 0.5f) {
+			transform.position += Vector3.Normalize (destination.position - transform.position) * Time.deltaTime * zombieMoveSpeed;
+		}
+	}
+
+}
