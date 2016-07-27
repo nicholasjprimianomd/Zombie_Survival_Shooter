080e6aafe930c0155db6770c3e7b5ae2f45ca7f4
Nicholas Primiano <njp280@nyu.edu>
2016-07-27 08:56:54 -0400

    no message

--- Assets/Scripts/PlayerAddHealth.cs
+++ Assets/Scripts/PlayerAddHealth.cs
@@ -4,23 +4,17 @@ using System;
 
 public class PlayerAddHealth : MonoBehaviour
 {
-
 	public Transform playerLocation;
 	public PlayerTakeDamage playerHealth;
 	public float pickUpDist = 5f;
 	public int healthToAdd = 25;
-
-	// Use this for initialization
-	void Start ()
-	{
-	
-	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if ((transform.position - playerLocation.position).magnitude <= pickUpDist) {
+		if ((transform.position - playerLocation.position).magnitude <= pickUpDist && playerHealth.currentHealth < playerHealth.maxHealth) {
 			playerHealth.currentHealth += healthToAdd;
+			gameObject.SetActive (false);
 		}
 	}
 }
--- Assets/Scripts/PlayerTakeDamage.cs
+++ Assets/Scripts/PlayerTakeDamage.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class PlayerTakeDamage : MonoBehaviour
 {
@@ -7,11 +8,17 @@ public class PlayerTakeDamage : MonoBehaviour
 	public int maxHealth = 100;
 	public int currentHealth = 100;
 
+	void Update ()
+	{
+		if (currentHealth > maxHealth) {
+			currentHealth = maxHealth;
+		}
+	}
+
 	void OnCollisionEnter2D (Collision2D coll)
 	{
 		if (coll.gameObject.tag == "Zombie") {
 			currentHealth -= 1;
 		}
-
 	}
 }
