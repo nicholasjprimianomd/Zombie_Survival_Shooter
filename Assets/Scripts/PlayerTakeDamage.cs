885ca71d7318ab550bc71aa67e561ccd0cfdf16a
Nicholas Primiano <njp280@nyu.edu>
2016-08-01 03:17:33 -0400

    no message

--- Assets/Scripts/PlayerTakeDamage.cs
+++ Assets/Scripts/PlayerTakeDamage.cs
@@ -2,29 +2,51 @@
 using System.Collections;
 using System;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class PlayerTakeDamage : MonoBehaviour
 {
 
 	public int maxHealth = 100;
 	public int currentHealth = 100;
+	public float maxShield = 5;
+	public float shieldRegenRate = 1;
+	public float currentShield = 100;
 
 	void Update ()
 	{
-		if (currentHealth > maxHealth) {
+		if (currentHealth >= maxHealth) {
 			currentHealth = maxHealth;
 		}
+
+		if (currentShield >= maxShield) {
+			currentShield = maxShield;
+		}
+			
 
 		if (currentHealth <= 0) {
 			SceneManager.LoadScene ("Death Scene");
 		}
 
+		incShield ();
 	}
 
-	void OnCollisionEnter2D (Collision2D coll)
+	void OnTriggerEnter2D (Collider2D coll)
 	{
 		if (coll.gameObject.tag == "Zombie") {
-			currentHealth -= 1;
+			print ("1");
+			if (currentShield > 0) {
+				currentShield -= 1;
+				print ("2");
+			} else {
+				currentHealth -= 1;
+				print ("3");
+			}
 		}
+	}
+
+	void incShield ()
+	{
+		currentShield += 1f * Time.deltaTime * shieldRegenRate;
 	}
 }
